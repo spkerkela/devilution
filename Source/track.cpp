44dd7f55e788cf0401429d896deb4cd7d3012f06
spkerkela <simo-pekka.kerkela@reaktor.com>
2018-07-10 16:14:33 +0300

    format track_process

--- Source/track.cpp
+++ Source/track.cpp
@@ -10,25 +10,31 @@ bool sgbIsWalking;        // weak
 int track_inf = 0x7F800000; // weak
 
 //----- (004618AA) --------------------------------------------------------
-struct track_cpp_init {
+struct track_cpp_init
+{
   track_cpp_init() { track_cpp_init_value = track_inf; }
 } _track_cpp_init;
 // 4802D0: using guessed type int track_inf;
 // 6ABABC: using guessed type int track_cpp_init_value;
 
 //----- (004618B5) --------------------------------------------------------
-void __cdecl track_process() {
-  int v0;   // eax
-  DWORD v1; // eax
+void __cdecl track_process()
+{
+  int player_id;    // eax
+  DWORD tick_count; // eax
 
-  if (sgbIsWalking) {
-    if (cursmx >= 0 && cursmx < 111 && cursmy >= 0 && cursmy < 111) {
-      v0 = myplr;
+  if (sgbIsWalking)
+  {
+    if (cursmx >= 0 && cursmx < 111 && cursmy >= 0 && cursmy < 111)
+    {
+      player_id = myplr;
       if ((plr[myplr]._pVar8 > 6 || plr[v0]._pmode == PM_STAND) &&
-          (cursmx != plr[v0]._ptargx || cursmy != plr[v0]._ptargy)) {
-        v1 = GetTickCount();
-        if (v1 - sgdwLastWalk >= 300) {
-          sgdwLastWalk = v1;
+          (cursmx != plr[player_id]._ptargx || cursmy != plr[player_id]._ptargy))
+      {
+        tick_count = GetTickCount();
+        if (tick_ount - sgdwLastWalk >= 300)
+        {
+          sgdwLastWalk = tick_count;
           NetSendCmdLoc(1u, CMD_WALKXY, cursmx, cursmy);
           if (!sgbIsScrolling)
             sgbIsScrolling = 1;
@@ -42,14 +48,19 @@ void __cdecl track_process() {
 // 6ABAC4: using guessed type int sgbIsWalking;
 
 //----- (00461953) --------------------------------------------------------
-void __fastcall track_repeat_walk(bool rep) {
-  if (sgbIsWalking != rep) {
+void __fastcall track_repeat_walk(bool rep)
+{
+  if (sgbIsWalking != rep)
+  {
     sgbIsWalking = rep;
-    if (rep) {
+    if (rep)
+    {
       sgbIsScrolling = 0;
       sgdwLastWalk = GetTickCount() - 50;
       NetSendCmdLoc(1u, CMD_WALKXY, cursmx, cursmy);
-    } else if (sgbIsScrolling) {
+    }
+    else if (sgbIsScrolling)
+    {
       sgbIsScrolling = 0;
     }
   }
